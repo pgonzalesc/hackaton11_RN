4c3dbec3d67b3a5353b2bff72d7c360040d30c18
pgonzalesc <pedro281195@gmail.com>
2020-12-19 09:24:07 -0500

    Se agrego el componente para el maps

---
+++ src/components/Maps/LocationMap/index.js
@@ -0,0 +1,76 @@
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, Platform, PermissionsAndroid} from 'react-native';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+import Geolocation from 'react-native-geolocation-service';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  maps: {...StyleSheet.absoluteFillObject},
+});
+
+const LocationMap = () => {
+  const [locations, setLocations] = useState([]);
+  let _watchId;
+  useEffect(() => {
+    if (Platform.OS === 'ios') {
+      Geolocation.requestAuthorization('always');
+    }
+    if (Platform.OS === 'android') {
+      PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      );
+    }
+    _watchId = Geolocation.watchPosition(
+      (position) => {
+        const {latitude, longitude} = position.coords;
+        setLocations([...locations, {latitude, longitude}]);
+      },
+      (error) => {
+        console.warn('error', error);
+      },
+      {
+        enableHighAccuracy: true,
+        distanceFilter: 100,
+        interval: 5000,
+        fastestInterval: 2000,
+      },
+    );
+  }, [locations]);
+
+  useEffect(() => {
+    return () => {
+      if (_watchId !== null) {
+        Geolocation.clearWatch(_watchId);
+      }
+    };
+  });
+  return (
+    <>
+      {locations.length > 0 && (
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.maps}
+          initialRegion={{
+            latitude: locations[0].latitude,
+            longitude: locations[0].longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}>
+          {locations.map((location, index) => (
+            <Marker
+              key={`location-${index}`}
+              coordinate={{
+                latitude: location.latitude,
+                longitude: location.longitude,
+              }}
+            />
+          ))}
+        </MapView>
+      )}
+    </>
+  );
+};
+
+export default LocationMap;
